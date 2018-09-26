99e9c18fae48e5d3204d7b05df4b97ebcb9a397f
That-Beaner <36551724+That-Beaner@users.noreply.github.com>
2018-09-26 12:55:46 -0500

    Update newScript.js

--- newScript.js
+++ newScript.js
@@ -1,1 +1,21 @@
-
+lines (226 sloc)  5.77 KB
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    $(document).ready(function(){
+        $("#mobileBody").css("display","block");
+        $("#body").css("display","none");
+        if(/iPhone|iPad|iPod/i.test(navigator.userAgent)){
+            $("#iphoneBody").css("display","block");
+        }
+    });
+}
+else
+{ 
+    let Usernames = [];
+    let NamingMethods = [];
+    let Installed = false;
+    let NumberOfBots = 20;
+    let LoadInline = true;
+    let HideCSS = false;
+    let CurrentVersion = 0;
+    let CurrentMethod = 0;
+ 
